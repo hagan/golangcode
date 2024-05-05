0793e9d6aacaf6c1d063da023dbb087a6df33e46
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-05-05 14:36:03 -0700

    add lessing 46

---
+++ 046-mod-code-depend-02/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	// also like this
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+}
