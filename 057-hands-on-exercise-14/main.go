9780a7a676ac2d863117f0a57889eba59357a52f
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-05-08 10:47:40 -0700

    more exercises

---
+++ 057-hands-on-exercise-14/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	fmt.Println(puppy.Bark())
+}
---
+++ 058-hands-on-exercise-15/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	puppy.From12()
+}
